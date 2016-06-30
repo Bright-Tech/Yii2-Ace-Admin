5d07607a2f6060f0796b79e9d6e0e15b067319ce
Sam.Xiao <samxiaotj@gmail.com>
2016-06-30 16:36:12 +0800

    更新 PageHeader

--- widgets/PageHeader.php
+++ widgets/PageHeader.php
@@ -12,14 +12,8 @@ class PageHeader extends Widget
     const PAGE_HEADER_SUB = 'subheader';
     public $title = 'Default Title';
     public $subTitle = '';
-    public $template = '<div class="page-header">
-    <h1><{title}>
-        <small><i class="ace-icon fa fa-angle-double-right"></i><{subtitle}></small>
-    </h1>
-</div>';
-    public $subHeaderTemplate = '<h3 class="header smaller lighter blue"><{title}>
-	<small><{subtitle}></small>
-</h3>';
+    public $headerTemplate = '<div class="page-header"><h1><{title}><{subTitle}></h1></div>';
+    public $subTitleTemplate = '<small><i class="ace-icon fa fa-angle-double-right"></i><{subTitle}></small>';
     public $type = 'header';
 
     public function init()
@@ -29,12 +23,12 @@ class PageHeader extends Widget
 
     public function run()
     {
-        $template = $this->template;
-        if ( $this->type == self::PAGE_HEADER_SUB ) {
-            $template = $this->subHeaderTemplate;
+        $subTitle = '';
+        if (!empty($this->subTitle)){
+            $subTitle = str_replace( '<{subTitle}>', Html::encode( $this->subTitle ), $this->subTitleTemplate );
         }
-        $html = str_replace( '<{title}>', Html::encode( $this->title ), $template );
-        $html = str_replace( '<{subtitle}>', Html::encode( $this->subTitle ), $html );
+        $html = str_replace( '<{title}>', Html::encode( $this->title ), $this->headerTemplate );
+        $html = str_replace( '<{subTitle}>', $subTitle, $html );
         return $html;
     }
 }
